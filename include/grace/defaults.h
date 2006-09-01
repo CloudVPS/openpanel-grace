0e460af9662bea5320c9f85b0175745f1f3a4031
Pim van Riezen <pi@panelsix.com>
2006-09-01 16:20:08 +0200

    Add unbalanced tag detection in configdb::load

--- include/grace/defaults.h
+++ include/grace/defaults.h
@@ -107,8 +107,10 @@ namespace defaults
 		parameter memoryalertcallback leakcallback defaultvalue (NULL);
 	}
 	
+	/// Settings for the xml parser.
 	namespace xml
 	{
+		/// Should the parser shriek on unbalanced tags?
 		parameter bool strictbalance defaultvalue (true);
 	}
 }
