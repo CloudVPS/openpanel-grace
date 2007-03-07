13e65890bc302b4c8e889f1486cd2651c85e80e0
Peter van Dijk <peter@panelsix.com>
2007-03-07 19:11:07 +0100

    try to fix dbfile operator= for const string

--- include/dbfile/dbfile.h
+++ include/dbfile/dbfile.h
@@ -113,6 +113,9 @@ public:
 							 /// string.
 	dbrecord				&operator= (string *o) { value v = o; fromvalue (v); return *this; }
 
+							 /// Assignment operator for a const string.
+	dbrecord 				&operator= (const string &o) { value v = o; fromvalue (v); return *this; }
+
 							 //@{
 							 /// Convenience access to underlying value.
 	const string			&sval (void) { return v.sval(); }
