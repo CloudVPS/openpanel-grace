77e30c009634ebe43700fa5c2fc8f6daec56ee77
pi <pi@Isaac.local>
2007-08-14 18:17:09 +0200

    IGN: hrng

--- src/libquerido/row.cpp
+++ src/libquerido/row.cpp
@@ -69,11 +69,9 @@ dbrow::dbrow (dbtable *tab, const statstring &pid)
 	
 	foreach (col, tmp[0])
 	{
-		::printf ("%s ", col.id().str());
 		dbcell *node = new dbcell (this, col.id(), col);
 		cells.set (col.id(), node);
 	}
-	::printf ("\n");
 }
 
 dbrow::~dbrow (void)
