ec869c8a3b8c453bcb8a9d632e6ef5552b2b7e91
pi <pi@Isaac.local>
2007-08-14 15:54:37 +0200

    IGN: debug

--- src/libquerido/table.cpp
+++ src/libquerido/table.cpp
@@ -101,6 +101,7 @@ void dbtable::commitrows (void)
 				}
 			}
 			qry.printf (" WHERE %s=\"%S\"", idxid.str(), row.id().str());
+			::printf ("commit -> %s\n", qry.str());
 			eng->query (qry);
 		}
 	}
