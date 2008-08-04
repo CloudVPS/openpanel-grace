a69d26d11ac400a048d3d5e628b810bc7847a335
Pim van Riezen <pi+hgcommit@openpanel.com>
2008-08-04 15:04:39 +0200

    Add gethostbyname_r tests

--- src/libgrace/netdb.cpp
+++ src/libgrace/netdb.cpp
@@ -7,11 +7,6 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-#ifndef gethostbyname_r
- #define gethostbyname_r gethostbyname
- #define gethostbyaddr_r gethostbyaddr
-#endif
-
 // =======================================================================
 // METHOD gethostbyname
 // --------------------
@@ -41,9 +36,10 @@
 // =======================================================================
 value *netdb::gethostbyname (const string &name)
 {
+#ifndef HAVE_GETHOSTBYNAME_R
 	struct hostent *he;
 	
-	he = ::gethostbyname_r (name.str());
+	he = ::gethostbyname (name.str());
 	if (! he)
 	{
 		returnclass (value) result retain;
@@ -51,6 +47,20 @@ value *netdb::gethostbyname (const string &name)
 		return &result;
 	}
 	return netdb::converthostentry (he);
+#else
+	struct hostent he;
+	struct hostent *result = NULL;
+	char addr_space[2560];
+	int err;
+	
+	if (gethostbyname_r (name.str(), &he, &addr_space, 2560, &result, &err))
+	{
+		returnclass (value) result retain;
+		result = false;
+		return &result;
+	}
+	return netdb::converthostentry (result);
+#endif
 }
 
 // ========================================================================
@@ -58,10 +68,11 @@ value *netdb::gethostbyname (const string &name)
 // ========================================================================
 ipaddress netdb::resolve (const string &name)
 {
+#ifndef HAVE_GETHOSTBYNAME_R
 	ipaddress result = 0;
 	struct hostent *he;
 	
-	he = ::gethostbyname_r (name.str());
+	he = ::gethostbyname (name.str());
 	if (! he) return result;
 	if (he->h_addrtype == AF_INET) // h_addr == h_addr_list[0];
 	{
@@ -71,6 +82,26 @@ ipaddress netdb::resolve (const string &name)
 	}
 	
 	return result;
+#else
+	struct hostent he;
+	struct hostent *result = NULL;
+	char addr_space[2560];
+	int err;
+	ipaddress resval = 0;
+	
+	if (gethostbyname_r (name.str(), &he, &addr_space, 2560, &result, &err))
+	{
+		return resval;
+	}
+	if (! result) return resval;
+	if (result->h_addrtype == AF_INET) 
+	{
+		struct in_addr &sin;
+		sin = ((struct in_addr *) result->h_addr_list[0]);
+		resval = ntohl (sin->s_addr);
+	}
+	return resval;
+#endif
 }
 
 // ========================================================================
@@ -78,6 +109,7 @@ ipaddress netdb::resolve (const string &name)
 // ========================================================================
 value *netdb::gethostbyaddr (const string &addr)
 {
+#ifndef HAVE_GETHOSTBYNAME_R
 	struct hostent *he;
 	struct in_addr ina;
 	
@@ -86,7 +118,7 @@ value *netdb::gethostbyaddr (const string &addr)
 		throw badAddressException();
 	}
 	
-	he = ::gethostbyaddr_r ((char *) &ina, sizeof (struct in_addr),
+	he = ::gethostbyaddr ((char *) &ina, sizeof (struct in_addr),
 							AF_INET);
 	if (! he)
 	{
@@ -95,6 +127,26 @@ value *netdb::gethostbyaddr (const string &addr)
 		return &result;
 	}
 	return netdb::converthostentry (he);
+#else
+	struct hostent he;
+	struct in_addr ina;
+	struct hostent_data he_data;
+	
+	if (!inet_aton (addr.str(), &ina))
+	{
+		throw badAddressException();
+	}
+	
+	if (gethostbyaddr_r ((char *) &ina, sizeof (struct in_addr),
+						 AF_INET, &he, &he_data));
+	{
+		returnclass (value) result retain;
+		result = false;
+		return &result;
+	}
+	
+	return netdb::converthostentry (&he);
+#endif
 }
 
 // ========================================================================
