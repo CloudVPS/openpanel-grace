5e189169e50bf541159ffa617a2bcf033b0bbca0
Pim van Riezen <pi@openpanel.com>
2010-03-30 23:12:30 +0200

    Exit runtest on first failure.

--- test/runtests.sh
+++ test/runtests.sh
@@ -3,7 +3,7 @@ echo "*** Building and running all test scenarios"
 DIRS=`ls -1 */runtest.sh | sed -e "s/.runtest.sh$//"`
 for dir in $DIRS; do
   cd $dir
-  ./runtest.sh && ( make clean >/dev/null )
+  ./runtest.sh && ( make clean >/dev/null ) || exit 1
   cd ..
 done
 echo "*** Quality control done"
