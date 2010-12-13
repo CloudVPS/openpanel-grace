3f5d350590b7f0d7f4a5f7ac26e4e8ca82b43c1c
Koert van der Veer <koert@openpanel.com>
2010-12-13 13:05:20 +0100

    Do not expose matrixSSL through the header

--- include/grace/sslsocket.h
+++ include/grace/sslsocket.h
@@ -13,71 +13,6 @@
 #include <grace/file.h>
 #include <grace/http.h>
 
-#include <matrixssl/matrixSsl.h>
-
-extern void setupMatrixSSL (void);
-
-/// Exception values emitted by sslclientcodec
-enum sslException
-{
-	EX_SSL_BUFFER_SNAFU		= 0xf95877be, ///< Buffer management error.
-	EX_SSL_PROTOCOL_ERROR	= 0xa3ecd69e, ///< SSL protocol related error.
-	EX_SSL_CLIENT_ALERT		= 0xe6950536, ///< MatrixSSL client alert.
-	EX_SSL_NO_HANDSHAKE		= 0xcc30b80b, ///< Action after failed handshake.
-	EX_SSL_INIT 			= 0x9fcc10c0  ///< Could not init SSL.
-};
-
-/// An iocodec implementing SSL client traffic through MatrixSSL.
-/// Used by sslsocket as the iocodec of choice.
-class sslclientcodec : public iocodec
-{
-public:
-					 sslclientcodec (void);
-					~sslclientcodec (void);
-	
-					 /// Initiates the SSL handshake.
-	bool			 setup (void);
-	
-					 /// Reset all buffers and handshake data.
-	void			 reset (void);
-	
-					 /// Adds SSL-coded data to the input buffer.
-	bool			 addinput (const char *, size_t);
-	
-					 /// Adds plain data to the output buffer for SSL coding.
-	bool			 addoutput (const char *, size_t);
-	
-					 /// Adds an SSL close event to the output buffer.
-	void			 addclose (void);
-	
-					 /// Loads decoded data into a ringbuffer.
-	bool			 fetchinput (ringbuffer &);
-	
-					 /// Copies currently encoded output data to a string.
-	void			 peekoutput (string &);
-	
-					 /// Used to report the number of coded output bytes
-					 /// that have been succesfully flushed.
-	void			 doneoutput (unsigned int);
-	
-					 /// Determines if there is enough room in the output
-					 /// buffer for a given number of bytes.
-	bool			 canoutput (unsigned int);
-	
-					 /// If called, certificate checking will be disabled.
-	void			 nocertcheck (void);
-	
-protected:
-	ssl_t			*ssl;
-	sslBuf_t		 inbuf;
-	sslBuf_t		 insock;
-	sslBuf_t		 outsock;
-	sslSessionId_t	 session;
-	bool			 disablecerts;
-	
-	bool			 handshakedone;
-};
-
 /// Implementation of tcpsocket with SSL support.
 /// Uses sslclientcodec to encapsulate with MatrixSSL.
 class sslsocket : public tcpsocket
@@ -112,22 +47,12 @@ class httpssocket : public httpsocket
 {
 public:
 					 /// Constructor.
-					 httpssocket (void)
-					 	: httpsocket ()
-					 {
-					 	_sock.codec = new sslclientcodec;
-					 }
+					 httpssocket (void);
 	
 					 /// Return any codec-related errors.
-	const string	&codecerror (void)
-					 {
-					 	return _sock.codec->error();
-					 }
+	const string	&codecerror (void);
 					 
-	void			 nocertcheck (void)
-					 {
-					 	_sock.codec->nocertcheck();
-					 }
+	void			 nocertcheck (void);
 };
 
 #endif
