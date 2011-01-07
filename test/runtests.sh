3ac33fe79cbced701a60632dfb60939ab434c855
Pim van Riezen <pi@openpanel.com>
2011-01-07 10:55:19 +0100

    Give runtests.sh a friendlier face.

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 echo "*** Building and running all test scenarios"
 DIRS=`ls -1 */runtest.sh | sed -e "s/.runtest.sh$//"`
+errors=0
 for dir in $DIRS; do
   cd $dir
-  ./runtest.sh && ( make clean >/dev/null ) || true
+  ./runtest.sh && ( make clean >/dev/null ) || errors=$((errors + 1))
+  if [ -e test.log ]; then cat test.log; fi
   cd ..
 done
-echo "*** Quality control done"
+if [ "$errors" = "0" ]; then
+  echo "*** Quality control done, no errors. Time to ship!"
+else
+  if [ "$errors" = "1" ]; then
+    echo "*** Quality control done, 1 error."
+  else
+    echo "*** Quality control done, $errors errors."
+  fi
+fi
