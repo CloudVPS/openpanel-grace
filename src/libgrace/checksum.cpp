958759e5dc5d59cf35daea3069e9d26debb76520
Koert van der veer <koert@xlshosting.nl>
2011-02-10 22:12:47 +0100

    Use new hash algorithm, removed unused algorithms

--- src/libgrace/checksum.cpp
+++ src/libgrace/checksum.cpp
@@ -9,136 +9,29 @@
 #include <sys/types.h>
 #include <grace/tolower.h>
 
-// ========================================================================
+// =============================================================================
 // FUNCTION checksum
-// taken after 'murmurhash' http://tanjent.livejournal.com/756623.html
-// ========================================================================
+// taken after 'DJBHash' http://www.partow.net/programming/hashfunctions/#DJBHashFunction
+// An algorithm produced by Professor Daniel J. Bernstein and shown first to the 
+// world on the usenet newsgroup comp.lang.c. It is one of the most efficient 
+// hash functions ever published. 
+// A test with an english dictionary (58K words) produced no collisions.
+// =============================================================================
 unsigned int checksum (const char *str)
 {
-#if __arm__
-	// FIXME: Ugly, ugly, ugly hack...
-	// somehow, the checkum or its consumers are incorrect on ARM. For now,
-	// use a different hash on ARM
+    unsigned int hash = 5381;
+    unsigned int i    = 0;
+    
+    const unsigned char* ustr = (const unsigned char*)str;
 
-	if (!str || !*str) return 0;
+    for(i = 0; ustr[i]; i++)
+    {
+        hash = ((hash << 5) + hash) + (ustr[i] & 0xDF);
+    }
 
-	unsigned int result = *str;
-	unsigned int shift = 0;
-
-	while (*(++str))
-	{
-		unsigned int c = *str;
-		int s = result % 31;
-	
-		result ^= (s << s);
-		result ^= (s << (s-32) );
-	}
-
-	return result;
-
-#else
-	
-	if (! str) return 0;
-	
-	const unsigned int m = 0x7fd652ad;
-	unsigned int h = 0xdeadbeef;
-	const int r = 16;
-	
-	const char *data = str;
-	
-	while (data[0] && data[1] && data[2] && data[3])
-	{
-		h += (*(unsigned int *) data) & 0xdfdfdfdf;
-		h *= m;
-		h ^= h >> r;
-		data += 4;
-	}
-	
-	if (*data)
-	{
-		h+= (*data & 0xdf); data++;
-		if (*data) { h+= (*data & 0xdf) << 8; data++; }
-		if (*data) { h+= (*data & 0xdf) << 16; data++; }
-		h *= m;
-		h ^= h >> r;
-	}
-	
-	h *= m;
-	h ^= h >> 10;
-	h *= m;
-	h ^= h >> 17;
-
-	return h;	
-#endif
+    return hash;
 }
 
-const unsigned char obox64[64] = {2,47,54,7,10,37,34,15,6,43,38,9,24,41,8,
-								  21,36,33,22,17,52,39,50,1,44,27,4,43,30,15,
-								  12,49,48,13,14,31,42,5,26,45,0,51,38,53,16,
-								  23,32,37,20,9,40,25,8,39,42,7,14,35,36,11,
-								  6,55,46,3};
-
-// ========================================================================
-// FUNCTION checksum64
-// ========================================================================
-unsigned long long checksum64 (const char *string)
-{
-	register int len, pos;
-	unsigned long long c;
-	len = strlen (string);
-	
-	register unsigned long long result = 0;
-	unsigned long long len8 = len & 0xff;
-	
-	result  = (len8 ^ 0x18) << 56;
-	result |= (len8 ^ 0x24) << 48;
-	result |= (len8 ^ 0x42) << 40;
-	result |= (len8 ^ 0x81) << 32;
-	result |= (len8 ^ 0x14) << 24;
-	result |= (len8 ^ 0x41) << 16;
-	result |= (len8 ^ 0x28) << 8;
-	result |= (len8 ^ 0x82);
-	
-	for (pos=0; pos<len; ++pos)
-	{
-		c = (unsigned long long) _tolower ((int) string[pos]);
-	}
-
-	for (pos=0;string[pos];++pos)
-	{
-		c = (unsigned long long) _tolower((int) string[pos]);
-		if (c & 0xc0)
-		{
-			result ^= ((((c & 0xdf)^obox64[(pos+7) & 0x3f]) & 0x3f) << obox64[pos & 0x3f]);
-			result ^= ((((c & 0xdf)^obox64[(len-(pos+7)) & 0x3f]) & 0x3f) << obox64[(len-pos) & 0x3f]);
-		}
-		else
-		{
-			result ^= (((c^obox64[(pos+17) & 0x3f]) & 0x3f) <<
-					   obox64[(pos^c) & 0x3f]);
-			result ^= (((c^obox64[((len-pos)+17) & 0x3f]) & 0x3f) <<
-					   obox64[((len-pos)^c) & 0x3f]);
-		}
-		result += (c * pos);
-	}
-	return result;
-}
-
-// ========================================================================
-// FUNCTION djbhash64
-// ========================================================================
-unsigned long long djbhash64 (const char *string)
-{
-	register int pos;
-	register unsigned long long result = 5381;
-	
-	for (pos=0; string[pos]; ++pos)
-	{
-		result = result * 33 ^ _tolower((int) string[pos]);
-	}
-	
-	return result;
-}
 
 char _TRANS_TABLE[128] =
 	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,63,
