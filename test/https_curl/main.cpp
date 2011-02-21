75898ebf272eb1c6b70b11471a56669e49695aad
Koert van der Veer <koert@openpanel.com>
2011-02-21 14:57:26 +0100

    Testcast for failing connect over SSLv2

---
+++ test/https_curl/main.cpp
@@ -0,0 +1,46 @@
+#include <grace/application.h>
+#include <grace/http.h>
+#include <grace/sslsocket.h>
+#include <grace/httpd.h>
+#include <grace/strutil.h>
+
+extern "C" void grace_init (void) { __THREADED = true; }
+
+class httpsCurlApp : public application
+{
+public:
+		 	 httpsCurlApp (void) :
+				application ("grace.testsuite.https")
+			 {
+			 }
+			~httpsCurlApp (void)
+			 {
+			 }
+
+	int		 main (void);
+};
+
+APPOBJECT(httpsCurlApp);
+
+void __breakme (void) {}
+
+#define debug_out if(1) ::printf
+
+int httpsCurlApp::main (void)
+{
+	httpsd srv (14265);
+	srv.systempath ("docroot");
+	srv.loadkeyfile("cert.pem");
+
+	httpdfileshare	srv_fshare (srv, "*", "docroot");
+	
+	srv.start ();
+
+	sleep(3);
+
+	ferr.printf ("Shutting down...\n");	
+	srv.shutdown();
+	ferr.printf ("Shutdown completed...\n");
+	return 0;
+}
+
---
+++ test/https_curl/runtest.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+testname=`echo "https                       " | cut -c 1-24`; $(which echo) -n "${testname}: "
+rm -f *.o default.* localhost.* >/dev/null 2>&1
+$(which echo) -n "."
+make clean >/dev/null 2>&1 || $(which echo) -n ""
+$(which echo) -n "."
+make > test.log 2>&1 || {
+  echo "   failed (BUILD)"
+  exit 1
+}
+$(which echo) -n "."
+echo "--- start run" >> test.log
+./https >> test.log 2>&1 &
+
+curl -k1 -m1 -otls1.txt https://localhost:14265/test.txt 2>/dev/null || {
+    echo " failed (RUN)"
+    exit 1
+}
+curl -k3 -m1 -ossl3.txt https://localhost:14265/test.txt 2>/dev/null || {
+    echo " failed (RUN)"
+    exit 1
+}
+curl -k2 -m1 -ossl2.txt https://localhost:14265/test.txt 2>/dev/null || {
+    echo " failed (RUN)"
+    exit 1
+}
+wait
+
+$(which echo) -n "."
+echo "--- start diff" >> test.log
+for file in ssl3.txt ssl2.txt tls1.txt; do
+  diff $file docroot/test.txt >> test.log 2>&1 || {
+    echo " failed (DIFF)"
+    exit 1
+  }
+done
+rm -rf ssl3.txt ssl2.txt tls1.txt http.app http
+echo " passed"
