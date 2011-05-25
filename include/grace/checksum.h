7d443ca4d65cb09cacc2d2f23a8c1dd9c490afc5
Koert van der Veer <koert@openpanel.com>
2011-05-25 14:37:52 +0200

    meh

--- include/grace/checksum.h
+++ include/grace/checksum.h
@@ -13,18 +13,6 @@
 /// \param str The string to be hashed.
 unsigned int checksum (const char *str);
 
-/// A 64 bits hash.
-/// Basically an extension of the 32 bits version, but more suitable for
-/// situations with millions of entries.
-/// \param str The string to be hashed.
-unsigned long long checksum64 (const char *str);
-
-/// A 64 bit hash.
-/// Implementation of the djb hash in a 64 bits variant. Gets more collisions
-/// but is 5-10% faster than checksum64.
-/// \param str The string to be hashed.
-unsigned long long djbhash64 (const char *str);
-
 /// OBSOLETE: convert string of four characters to a 24 bit integer.
 /// \param str The string.
 unsigned int resid (const char *str);
