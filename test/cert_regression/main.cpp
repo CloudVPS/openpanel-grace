9e70c80e22df0e505025dca9d29f4ff440486198
Koert van der Veer <koert@openpanel.com>
2011-07-15 16:57:55 +0200

    Added unit test for certificate issues (untested, though)

---
+++ test/cert_regression/main.cpp
@@ -0,0 +1,60 @@
+#include <grace/application.h>
+#include <grace/http.h>
+#include <grace/sslsocket.h>
+#include <grace/httpd.h>
+#include <grace/strutil.h>
+
+extern "C" void grace_init (void) { __THREADED = true; }
+
+class httpsApp : public application
+{
+public:
+		 	 httpsApp (void) :
+				application ("grace.testsuite.https")
+			 {
+			 }
+			~httpsApp (void)
+			 {
+			 }
+
+	int		 main (void);
+};
+
+APPOBJECT(httpsApp);
+
+void __breakme (void) {}
+
+#define debug_out if(1) ::printf
+
+int httpsApp::main (void)
+{
+	value public_local;
+	httpsd 			srv (4269);
+					srv.systempath ("docroot");
+
+	srv.loadkeyfile("brokencert.pem");
+	srv.start ();
+
+	httpssocket hs;
+
+	hs.nocertcheck();
+	hs.keepalive(false);
+
+	debug_out( "get https://localhost:4269/public.dat\n");
+
+	public_local = hs.get ("https://localhost:4269/public.dat");
+	if (! public_local.strlen ())
+	{
+		ferr.printf ("FAIL public local\n");
+		ferr.printf ( hs.error );
+		ferr.printf ( ".\n" );
+		return 2;
+	}
+
+	fs.save ("localhost", public_local);
+
+	ferr.printf ("Shutting down...\n");
+	srv.shutdown();
+	ferr.printf ("Shutdown completed...\n");
+	return 0;
+}
---
+++ test/cert_regression/runtest.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+testname=`echo "https                       " | cut -c 1-24`; $(which echo) -n "${testname}: "
+
+rm -f *.o default.* localhost.* >/dev/null 2>&1
+echo -n "."
+
+make clean >/dev/null 2>&1 || echo -n ""
+echo -n "."
+
+make > test.log 2>&1 || {
+  echo "   failed (BUILD)"
+  exit 1
+}
+
+$(which echo) -n "."
+echo "--- start run" >> test.log
+./https >> test.log 2>&1 || {
+  echo "  failed(RUN)"
+  exit 1
+}
+
+$(which echo) -n "."
+echo "--- start diff" >> test.log
+
+diff localhost reference.localhost >> test.log 2>&1 || {
+  echo " failed (DIFF)"
+  exit 1
+}
+
+rm -rf test.log localhost https.app https https.exe
+echo " passed"
